8509388e30c7949618ff0bbe316abb931139f2da
22-2 <17890m@protonmail.ch>
2025-11-29 11:35:56 +0900

    fix

--- src/workers/clock.worker.ts
+++ src/workers/clock.worker.ts
@@ -1,12 +1,12 @@
 // Worker: sends current timestamp every second when it receives 'start', stops on 'stop'
 self.onmessage = function (e) {
 	if (e && e.data === "start") {
-		// @ts-ignore - attach to self
+		// @ts-expect-error - attach to self
 		self._timer = setInterval(function () {
 			self.postMessage({ now: Date.now() });
 		}, 1000);
 	} else if (e && e.data === "stop") {
-		// @ts-ignore
+		// @ts-expect-error
 		if (self._timer) {
 			// @ts-expect-error
 			clearInterval(self._timer);
